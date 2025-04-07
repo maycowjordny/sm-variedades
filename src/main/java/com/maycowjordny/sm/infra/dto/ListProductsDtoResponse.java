113ed48757e34d53c4a579a9fc211d1ac2656645
maycow jordny <maycowjordny2015@gmail.com>
2025-04-07 14:05:44 -0300

    fix: add session ao productDTO

--- src/main/java/com/maycowjordny/sm/infra/dto/ListProductsDtoResponse.java
+++ src/main/java/com/maycowjordny/sm/infra/dto/ListProductsDtoResponse.java
@@ -1,21 +1,24 @@
 package com.maycowjordny.sm.infra.dto;
 
 
-import com.maycowjordny.sm.domain.model.Product;
-import lombok.Data;
-
 import java.math.BigDecimal;
 import java.util.UUID;
+
+import com.maycowjordny.sm.domain.model.Product;
+
+import lombok.Data;
 
 @Data
 public class ListProductsDtoResponse {
     private UUID id;
     private String name;
     private BigDecimal price;
+    private Integer session;
 
     public ListProductsDtoResponse(Product product) {
         this.id = product.getId();
         this.name = product.getName();
         this.price = product.getPrice();
+        this.session = product.getSession();
     }
 }
