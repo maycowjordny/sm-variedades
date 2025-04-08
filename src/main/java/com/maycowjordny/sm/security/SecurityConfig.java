5547aada686866f379a1de40af29a773faa0b0a1
maycow jordny <maycowjordny2015@gmail.com>
2025-04-08 11:28:10 -0300

    feat: add filtro jwt

--- src/main/java/com/maycowjordny/sm/security/SecurityConfig.java
+++ src/main/java/com/maycowjordny/sm/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://sm-variedades.netlify.app/"));
         config.setAllowCredentials(true);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
